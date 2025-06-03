7766843d09264b993b769297a6ad4005d3969f0c
kaipov24 <kaipovich24@gmail.com>
2025-06-03 22:22:36 +0600

    Logger

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/kaipov24/reelingit/logger"
+)
+
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movie.log")
+	if err != nil {
+		log.Fatalf("Failed to initializelogger %v", err)
+	}
+	defer logInstance.Close()
+	return logInstance
+}
+
 func main() {
 
+	logInstance := initializeLogger()
+
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	const addr = ":8080"
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatalf("Server failed: %v", err)
+		logInstance.Error("Server failed", err)
+	}
 }
