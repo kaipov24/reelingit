8d3908254ff71b982100825ae87c72e6da14895f
kaipov24 <kaipovich24@gmail.com>
2025-06-04 13:47:52 +0600

    Json response

--- handlers/movie_handlers.go
+++ handlers/movie_handlers.go
@@ -10,6 +10,13 @@ import (
 type MovieHandler struct {
 }
 
+func (h *MovieHandler) writeJSONResponse(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
 func (h *MovieHandler) GetTopMovies(w http.ResponseWriter, r *http.Request) {
 	movies := []models.Movie{
 		{
@@ -40,8 +47,38 @@ func (h *MovieHandler) GetTopMovies(w http.ResponseWriter, r *http.Request) {
 			Casting:     []models.Actor{{ID: 3, FirstName: "Lara Hunt"}},
 		},
 	}
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(movies); err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	h.writeJSONResponse(w, movies)
+}
+
+func (h *MovieHandler) GetRandomMovies(w http.ResponseWriter, r *http.Request) {
+	movies := []models.Movie{
+		{
+			ID:          1,
+			TMDB_ID:     101,
+			Title:       "The Hacker",
+			ReleaseYear: 2022,
+			Genres:      []models.Genre{{ID: 1, Name: "Thriller"}},
+			Keywords:    []string{"hacking", "cybercrime"},
+			Casting:     []models.Actor{{ID: 1, FirstName: "Jane Doe"}},
+		},
+		{
+			ID:          2,
+			TMDB_ID:     102,
+			Title:       "Space Dreams",
+			ReleaseYear: 2020,
+			Genres:      []models.Genre{{ID: 2, Name: "Sci-Fi"}},
+			Keywords:    []string{"space", "exploration"},
+			Casting:     []models.Actor{{ID: 2, FirstName: "John Star"}},
+		},
+		{
+			ID:          3,
+			TMDB_ID:     103,
+			Title:       "The Lost City",
+			ReleaseYear: 2019,
+			Genres:      []models.Genre{{ID: 3, Name: "Adventure"}},
+			Keywords:    []string{"jungle", "treasure"},
+			Casting:     []models.Actor{{ID: 3, FirstName: "Lara Hunt"}},
+		},
 	}
+	h.writeJSONResponse(w, movies)
 }
